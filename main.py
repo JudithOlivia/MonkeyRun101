893281993260951299e16bdca8a0fc68009cef4c
Judith Olivia <jxdith13@gmail.com>
2025-09-14 00:06:25 -0400

    screen

--- main.py
+++ main.py
@@ -12,13 +12,22 @@ pygame.display.set_caption("Monket Run")
 WIDTH, HEIGHT = 800, 400
 FPS = 60
 
-score = 0
-high_score = 0
-show_new_high_score = Falseshow_new_hogh_score_time = 0
-SCORE_RATE = 5 
 
 GREEN = (34, 177, 76)
 WHITE = (255, 255, 255)
+BROWN = (139, 69, 19)
+RED = (255, 0, 0)
+GRAY = (120, 120, 120)
+BLUE = (0, 0, 255)
+BLACK = (0, 0, 0)
+
+GROUND_HEIGHT = 60
+PLAYER_WIDTH, PLAYER_HEIGHT = 40, 60
+INIT_SPEED = 4
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+
 
 
 
